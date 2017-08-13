ed068d9cbd0613454c73a8af4d39fb77390fa2a2
Yuchen Zhong <yzhong.cs@gmail.com>
2017-08-12 20:51:39 -0400

    Initial Commit

---
+++ PresentationStyle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PresentationStyle
+//
+//  Created by Yuchen Zhong on 2017-08-12.
+//  Copyright © 2017 Yuchen Zhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
